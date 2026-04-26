2c4054f1dce714f52f75348c1eeee46ccf1c4fbd
Gabe <contatogabemelo@gmail.com>
2026-04-26 12:50:24 -0300

    feat(landing): add Problema section with problem cards and impact stats

--- landing_page/src/App.tsx
+++ landing_page/src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from './components/layout/Navbar'
 import Hero from './components/sections/Hero'
+import Problema from './components/sections/Problema'
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <Problema />
       </main>
     </>
   )
